42b172426c4842c33d973c767f052d18a4684f28
sarapiresds <157735964+sarapiresds@users.noreply.github.com>
2024-08-07 16:06:08 -0300

    Update demo.js

--- website/pages/en/demo.js
+++ website/pages/en/demo.js
@@ -147,7 +147,7 @@ class Docs extends React.Component {
       <div className="roteiro">
              <h2>Roteiro para auxiliar na navegação dos ambientes</h2>
             <div className="demo-link">
-                <a href="https://ieducar.org/demo.html">Baixar</a>
+                <a href="https://github.com/portabilis/i-educar-website/issues/84#issue-2454145997">Baixar</a>
               </div>
             </div>
             
